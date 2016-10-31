dc144e88b4f9a3491058a032801b1339159eff5c
Nicholas Ellul <nicholas.ellul1@gmail.com>
2016-10-31 13:30:42 -0400

    Refactored Code

--- src/Stub.java
+++ src/Stub.java
@@ -24,23 +24,15 @@ public class Stub {
  public static void main(String[] args)
  {
    
-  Stack towerA = new Stack();
-  Stack towerB = new Stack();
-  Stack towerC = new Stack();
+  String towerA = "A";
+  String towerB = "B";
+  String towerC = "C";
   int TILES = 3;
-		  
-  for(int counter = TILES ; counter <= TILES; counter ++){
-  towerA.push(counter);
-  System.out.println(counter);
-  }
+	
   
-  henoi(TILES,"A","B","C");
+  henoi(TILES,towerA,towerB,towerC);
   
-  for(int counter = TILES; counter <= TILES; counter ++){
-  
-  System.out.println(towerC.push(counter));
-  }
-  
+ 
  }
-
 }
+ 
